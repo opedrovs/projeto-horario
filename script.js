9ea15347803462f52b3924878b503948ca8ff881
Yama <92165034+pvsanches@users.noreply.github.com>
2022-05-13 19:11:54 -0300

    adicionando função de zero a esquerda

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 function carregar() {
+    function zeroAEsquerda(num) {
+        return num >= 10 ? num : `0${num}`
+    }
+
     let horario = window.document.getElementById('horario')
     let data = new Date()
-    let hora = data.getHours()
-    let min = data.getMinutes()
-    let seg = data.getSeconds()
+    let hora = zeroAEsquerda(data.getHours())
+    let min = zeroAEsquerda(data.getMinutes())
+    let seg = zeroAEsquerda(data.getSeconds())
 
     horario.innerHTML = `<h1>${hora} : ${min} : ${seg}</h1>` 
 }
